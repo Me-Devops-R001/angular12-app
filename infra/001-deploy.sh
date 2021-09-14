12044153a42a0a7525722246379f169f2125ffc9
TyagiAmitYahooMail <tyagiamit1975@yahoo.com>
2021-09-15 00:09:16 +0530

    Updated the bash script for proper execution

--- infra/001-deploy.sh
+++ infra/001-deploy.sh
@@ -1,61 +1,54 @@
 #!/bin/bash
 
+read -p "Input the name of the new docker image?  " imagename
 read -p "Input the version number of new docker image?  " imageversion
-read -p "Verify the name & version number of new docker image: (atangularapp12ui:$imageversion) (Y/N)? " proceed
-    ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request......."; exit 1 ;}
+
+# read -p "Verify the name & version number of new docker image: (atangularapp12ui:$imageversion) (Y/N)? " proceed
+#     ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request......."; exit 1 ;}
+read -p "Proceed to build the docker image (all existing images with name & tag ($imagename:$imageversion) \
+ and all their running containers will be deleted) (Y/N)?   " proceed
+    ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request ..."; exit 1 ;}
+# f you invoke the exit in a subshell, it will not pass variables to the parent. Use { and } instead of ( and ) if you do not want Bash to fork a subshell.
+
+#if ( -z [ docker images --all | grep -wi 'atangularapp12ui' | grep -wi $imageversion ] )
+#   then
+        echo "Deleting existing image with same name and version (if exist) ..."
+        docker image rm amtayaji/$imagename:$imageversion --force
+        docker image rm $imagename:$imageversion --force
+#    fi
 
 cd ./frontend/
-
-read -p "Proceed to build the docker image (all existing images with name & tag (atangularapp12ui:$imageversion) and their running containers will be deleted) (Y/N)?   " proceed
-    ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request......."; exit 1 ;}
-    if [ -n [ docker images --all | [ grep "atangularapp12ui" &&  grep "$imageversion" ]]]
-    then
-        echo "Deleting existing image with same name and version..."
-        docker image rm amtayaji/atangularapp12ui:$imageversion --force
-        docker image rm atangularapp12ui:$imageversion --force
-    fi
-
-read -p "Creating new image with same name and version..."
-    docker build . --tag atangularapp12ui:$imageversion --file Dockerfile-bootstrap-server-host
+read -p "Creating new image with name and version : ($imagename:$imageversion) ..." dudkey
+    docker build . --tag $imagename:$imageversion --file Dockerfile-bootstrap-server-host
 
 read -p "Do you want to see list of the local docker images (Y/N)?  " listimages
-    if [ $listimages = "Y" ]
-    then
-        docker images
-    fi
+    ([ $listimages = "Y" ] || [ $listimages = "y" ]) && ( docker images ;)
 
 read -p "Do you want to instantiate a test container (Y/N)? " crearecontainer
-    if [ $crearecontainer = "Y" ]
+    if ([ $crearecontainer = "Y" ] || [ $crearecontainer = "y" ])
     then
-        docker container run -d --rm -p 4000:4000 --name at-test-atangularapp12ui-$imageversion atangularapp12ui:$imageversion
+        docker container run -d --rm -p 4000:4000 --name at-test-$imagename-$imageversion $imagename:$imageversion
+        read -p "Container created with Name = at-test-$imagename-$imageversion"
         docker container ls
         read -p "Do you wish to bash into this container (Y/N)? " bashintoit
-            if [ $bashintoit = "Y" ]
-            then
-                docker exec --interactive --tty bash
-            fi
+            ([ $bashintoit = "Y" ] || [ $bashintoit = "y" ]) && { docker exec --interactive --tty at-test-$imagename-$imageversion bash -c "echo 'Hello Tyagi'";}
     fi
-
-
 
 read -p "Image creation complete, Do you wish to deploy it to Docker Hub (Y/N)? " dodeployment
-    if [ $dodeployment = "N" ]
-    then
-        exit
-    fi
+    ([ $dodeployment = "N" ] || [ $dodeployment = "n" ]) && { echo "Execution terminated on user request ..."; exit 1 ;}
 
 # HOSTING COMMANDS BEGIN ...........................................................................................
 cd ../hosting/
 
-read -p "Please login to Docker Hub with credentials below..."
-    docker login --username "amtayaji" --password-stdin
-    # docker login --username "amtayaji" --password amit#@+1
+read -p "Please login to Docker Hub with credentials below..." dudkey
+    # docker login --username "amtayaji" --password-stdin
+    docker login --username "amtayaji" --password "amit#@+1"
 
-read -p "Tagging the image as: atangularapp12ui:$imageversion ..."
-    docker tag atangularapp12ui:$imageversion amtayaji/atangularapp12ui:$imageversion
+read -p "Tagging the image as: $imagename:$imageversion ..." dudkey
+    docker tag $imagename:$imageversion amtayaji/$imagename:$imageversion
 
-read -p "Pushing the tagged image to Docker Hub..."
-    docker push amtayaji/atangularapp12ui:$imageversion
+read -p "Pushing the tagged image to Docker Hub..." dudkey
+    docker push amtayaji/$imagename:$imageversion
 # HOSTING COMMANDS END ...........................................................................................
 
 cd ..
