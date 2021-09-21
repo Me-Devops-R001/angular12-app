7e22300b57863a76e43746597f3ccf26a1e9ba14
TyagiAmitYahooMail <tyagiamit1975@yahoo.com>
2021-09-21 14:15:13 +0530

    Working copy with latest updation before removing local server

--- infra/001-deploy.sh
+++ infra/001-deploy.sh
@@ -3,19 +3,14 @@
 read -p "Input the name of the new docker image?  " imagename
 read -p "Input the version number of new docker image?  " imageversion
 
-# read -p "Verify the name & version number of new docker image: (atangularapp12ui:$imageversion) (Y/N)? " proceed
-#     ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request......."; exit 1 ;}
 read -p "Proceed to build the docker image (all existing images with name & tag ($imagename:$imageversion) \
  and all their running containers will be deleted) (Y/N)?   " proceed
     ([ $proceed = "N" ] || [ $proceed = "n" ]) &&  { echo "Execution terminated on user request ..."; exit 1 ;}
 # f you invoke the exit in a subshell, it will not pass variables to the parent. Use { and } instead of ( and ) if you do not want Bash to fork a subshell.
 
-#if ( -z [ docker images --all | grep -wi 'atangularapp12ui' | grep -wi $imageversion ] )
-#   then
-        echo "Deleting existing image with same name and version (if exist) ..."
-        docker image rm amtayaji/$imagename:$imageversion --force
-        docker image rm $imagename:$imageversion --force
-#    fi
+echo "Deleting existing image with same name and version (if exist) ..."
+docker image rm amtayaji/$imagename:$imageversion --force
+docker image rm $imagename:$imageversion --force
 
 cd ./frontend/
 read -p "Creating new image with name and version : ($imagename:$imageversion) ..." dudkey
@@ -37,9 +32,7 @@ read -p "Do you want to instantiate a test container (Y/N)? " crearecontainer
 read -p "Image creation complete, Do you wish to deploy it to Docker Hub (Y/N)? " dodeployment
     ([ $dodeployment = "N" ] || [ $dodeployment = "n" ]) && { echo "Execution terminated on user request ..."; exit 1 ;}
 
-# HOSTING COMMANDS BEGIN ...........................................................................................
-cd ../hosting/
-
+# IMAGE PROCESSING COMMANDS BEGIN ...........................................................................................
 read -p "Please login to Docker Hub with credentials below..." dudkey
     # docker login --username "amtayaji" --password-stdin
     docker login --username "amtayaji" --password "amit#@+1"
@@ -49,7 +42,19 @@ read -p "Tagging the image as: $imagename:$imageversion ..." dudkey
 
 read -p "Pushing the tagged image to Docker Hub..." dudkey
     docker push amtayaji/$imagename:$imageversion
-# HOSTING COMMANDS END ...........................................................................................
+# IMAGE PROCESSING COMMANDS END ...........................................................................................
+
+# AZURE HOSTING COMMANDS BEGIN ...........................................................................................
+cd ../hosting/
+kubectl delete --filename atangularapp12ui-service.yaml
+kubectl delete --filename atangularapp12ui-deployment.yaml
+
+kubectl apply --filename atangularapp12ui-deployment.yaml
+kubectl create --filename atangularapp12ui-service.yaml
+
+# AZURE HOSTING COMMANDS BEGIN ...........................................................................................
 
 cd ..
 exit 0;
+
+# at-ng-app12ui
